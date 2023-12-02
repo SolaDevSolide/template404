92e758f87ea8aff52f4b189f68e09e2fe09dd50a
SolaDevSolide <115653357+SolaDevSolide@users.noreply.github.com>
2023-12-02 22:57:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector('.slider');
+    let slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+    let initalSlideCount = slides.length;
+    let isCloningRequired = true;
+
+    function getVisibleSlides() {
+        return window.innerWidth >= 768 ? 2 : 1;
+    }
+
+    function setSlidesWidth() {
+        const containerWidth = slider.offsetWidth;
+        const margin = 10; // Margin between slides
+        const visibleSlides = getVisibleSlides();
+        const totalMargin = margin * 2 * (visibleSlides);
+        const slideWidth = (containerWidth - totalMargin) / visibleSlides;
+
+        slides.forEach(slide => {
+            slide.style.width = `${slideWidth}px`;
+            slide.style.marginRight = `${margin}px`;
+            slide.style.marginLeft = `${margin}px`;
+        });
+
+        // slides[slides.length - 1].style.marginRight = '0'; // Remove margin from the last slide
+
+        adjustSliderPosition();
+    }
+
+    function adjustSliderPosition() {
+        const slideWidth = slides[0].offsetWidth;
+        const slideMargin = 10 * 2; // Margin for each slide
+        const totalSlideWidth = slideWidth + slideMargin; // Total width including margin
+        slider.style.transform = `translateX(-${currentSlide * totalSlideWidth}px)`;
+    }
+
+    function cloneSlides() {
+        for (let i = 0; i < initalSlideCount; i++) {
+            const cloneSlide = slides[i].cloneNode(true);
+            slider.appendChild(cloneSlide);
+        }
+        slides = document.querySelectorAll('.slide'); // Update the slides NodeList
+    }
+
+    function nextSlide() {
+        const visibleSlides = getVisibleSlides();
+        currentSlide++;
+
+        if ((currentSlide + 1) % initalSlideCount == 0) {
+            isCloningRequired = true;
+        }
+
+        if (isCloningRequired){
+            cloneSlides();
+            isCloningRequired = false;
+        }
+
+        adjustSliderPosition();
+    }
+
+    window.addEventListener('resize', () => {
+        setSlidesWidth();
+        isCloningRequired = true; // Reset flag to clone again if needed
+    });
+
+    if (isCloningRequired) {
+        cloneSlides();
+        isCloningRequired = false;
+    }
+    setSlidesWidth();
+    setInterval(nextSlide, 3000);
+});
